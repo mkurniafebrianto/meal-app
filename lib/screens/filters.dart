a70bfb9d2a9a3c0fc0e0b54da15295067967f1b2
mkurniafebrianto <mkurniafebrianto@gmail.com>
2024-01-05 20:39:12 +0700

    style(filtersscreen): create filtersscreen

---
+++ lib/screens/filters.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+// import 'package:meals/screens/tabs.dart';
+// import 'package:meals/widgets/main_drawer.dart';
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutterFreeFilterSet = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
+      // drawer: MainDrawer(
+      //   onSelectScreen: (identifier) {
+      //     Navigator.of(context).pop();
+      //     if (identifier == 'meals') {
+      //       Navigator.of(context).push(
+      //         MaterialPageRoute(
+      //           builder: (ctx) => const TabsScreen(),
+      //         ),
+      //       );
+      //     }
+      //   },
+      // ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: _glutterFreeFilterSet,
+            onChanged: (isChecked) {
+              setState(() {
+                _glutterFreeFilterSet = isChecked;
+              });
+            },
+            title: Text(
+              'Glutten-free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include glutten-free meals.',
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
