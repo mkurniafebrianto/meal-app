ad1fafbd306a2b1bb81f85a0246de89f59cb2ba2
mkurniafebrianto <mkurniafebrianto@gmail.com>
2024-01-06 16:18:50 +0700

    feat(filter): add logic to filter meals

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -9,13 +9,15 @@ class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({
     super.key,
     required this.onToggleFavorite,
+    required this.availableMeals,
   });
 
   final void Function(Meal meal) onToggleFavorite;
+  final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
     // filter the meals that corresponding to certain category
-    final filteredMeals = dummyMeals
+    final filteredMeals = availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
