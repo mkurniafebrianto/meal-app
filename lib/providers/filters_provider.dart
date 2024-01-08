3283bd20bcc7cf4697b24366358ec628f704d675
mkurniafebrianto <mkurniafebrianto@gmail.com>
2024-01-08 13:20:31 +0700

    refactor(state-management): connect meals provider to filters provider

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/providers/meals_provider.dart';
 
 enum Filter {
   glutenFree,
@@ -25,7 +26,7 @@ class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
     // mutating state here is not allowed
     state = {
       ...state,
-      filter: isActive, 
+      filter: isActive,
     };
   }
 }
@@ -33,3 +34,24 @@ class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
 final filterProvider =
     StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
         (ref) => FiltersNotifier());
+
+final filteredMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final activeFilters = ref.watch(filterProvider);
+
+  return meals.where((meal) {
+    if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+      return false;
+    }
+    if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+      return false;
+    }
+    if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+    if (activeFilters[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+    return true;
+  }).toList();
+});
